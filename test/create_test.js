8dd660a53d74ad17d5a6a4da73cbf974f5561084
Richard Brooks <rickbrooks22@gmail.com>
2018-08-06 07:14:35 -0400

    added before statement to test helper

--- test/create_test.js
+++ test/create_test.js
@@ -2,10 +2,15 @@ const assert = require('assert');
 const User = require ('../src/user');
 
 describe('Creating records', () => {
-    it('saves a user', () => {
+    it('saves a user', (done) => {
         const rick = new User({ name: 'Rick' });
 
-        rick.save();
+        rick.save()
+            .then(() => {
+                // Has Rick been saved succcessfully?
+                assert(!rick.isNew);
+                done();
+            });
     });
 
 });
